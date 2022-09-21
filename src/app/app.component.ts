61b8a6dae003407da19ca5bed8712cb48d1880b5
Matias Lugo <mlugo@algeiba.com>
2022-09-21 20:27:24 -0300

    Fix in login presentations component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { LoginComponent } from './components/login/login.component';
 import { Message } from './entities/Message';
+import { AuthenticationService } from './services/authentication.service';
 import { BaseService } from './services/base.service';
 
 @Component({
@@ -13,20 +15,27 @@ import { BaseService } from './services/base.service';
 
 export class AppComponent implements OnInit{
   user!: SocialUser;
-  constructor(private baseService: BaseService,private socialAuthService: SocialAuthService,private router: Router) {
+  returnUrl = '/presentations';
+
+  constructor(private baseService: BaseService,private socialAuthService: SocialAuthService, private authService: AuthenticationService, private router: Router,private jwtHelper: JwtHelperService,) {
   }
 
   title = '';
 
   ngOnInit(): void {
-    this.socialAuthService.authState
-    .subscribe(res => {
-      this.user = res;
-    })  }
+      this.socialAuthService.authState
+      .subscribe(res => {
+        this.user = res;
+        if(res != null){
+          this.router.navigate([this.returnUrl]);
+        }
+      })
+  }
 
   signOut(): void {
     this.socialAuthService.signOut();
     this.router.navigate(['/']);
   }
+
 }
 
